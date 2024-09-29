f94357da8a6c608a7273ff5dafae24a35fb69838
Masoud S.R <amir.sepehr72@gmail.com>
2024-09-29 13:29:31 +0330

    some deploy optimizations

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const { PeerServer } = require("peer");
 
-const customGenerationFunction = () =>
-	(Math.random().toString(36) + "0000000000000000000").substr(2, 8);
+const customGenerationFunction = () => (Math.random().toString(36) + "0000000000000000000").substr(2, 8);
 
-const peerServer = PeerServer({ port: 9000, path: "/myapp" , generateClientId: customGenerationFunction });
+const peerServer = PeerServer({
+	port: process.env.PORT || 9000,
+	path: "/xo",
+	generateClientId: customGenerationFunction,
+});
